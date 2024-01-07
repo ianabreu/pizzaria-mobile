6a66b32b0eef4ff503762a46b2b4c1961e819163
ianabreu <ianabreudev@gmail.com>
2024-01-07 19:59:03 -0300

    styles: add themes colors, metrics and fonts files

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+import type { COLOR_TYPE } from "../@types/theme";
+
+export const colors: COLOR_TYPE = {
+  background: "#101026",
+  foreground: "#FFFFFF",
+
+  input: "#1d1d2e",
+  placeholder: "#f0f0f0",
+
+  primary: "#ff3f4b",
+  secondary: "#3fffa3",
+  terciary: "#3fd1ff",
+  cancel: "#5a5a5a",
+};
---
+++ src/theme/fonts.ts
@@ -0,0 +1,17 @@
+import { FONTS_TYPE } from "../@types/theme";
+
+export const fonts: FONTS_TYPE = {
+  family: {
+    regular: "Merriweather-Regular",
+    bold: "Merriweather-Bold",
+    styled: "Merienda-Bold",
+  },
+  size: {
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+    xxl: 22,
+    xxxl: 26,
+  },
+};
