eef9fb0c7603b78ad6248a7bedeebdf5e7b5e493
ianabreu <ianabreudev@gmail.com>
2024-01-11 05:35:19 -0300

    feat: add Logo component

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,14 +1,25 @@
-import { Image, Pressable } from "react-native";
+import { Image, Pressable, StyleSheet } from "react-native";
+interface LogoProps {
+  mb?: number;
+  height?: number;
+}
 
-function Logo() {
+export function Logo({ mb = 18, height = 100 }: LogoProps) {
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "auto",
+      marginBottom: mb,
+    },
+    image: {
+      width: "100%",
+      height: height,
+      objectFit: "contain",
+    },
+  });
   return (
-    <Pressable style={{ width: "100%" }}>
-      <Image
-        source={require("../../assets/logo.png")}
-        style={{ width: "100%", height: 80, objectFit: "contain" }} // Verifique se 'background' está correto
-      />
+    <Pressable style={styles.container}>
+      <Image source={require("../../assets/logo.png")} style={styles.image} />
     </Pressable>
   );
 }
-
-export default Logo;
