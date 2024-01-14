2584c37c00e6569a46d878d6df772d1c1d47f714
ianabreu <ianabreudev@gmail.com>
2024-01-14 03:33:06 -0300

    style: add prop size to ButtonProps on Button ui component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,23 +9,29 @@ import { colors, fonts, metrics } from "../../theme";
 import { ReactNode } from "react";
 
 interface ButtonProps extends TouchableOpacityProps {
+  size?: "small" | "large";
   isLoading?: boolean;
   children?: ReactNode;
 }
-export function Button({ children, isLoading = false, ...rest }: ButtonProps) {
+export function Button({
+  size = "small",
+  children,
+  isLoading = false,
+  ...rest
+}: ButtonProps) {
   const styles = StyleSheet.create({
     button: {
-      width: "100%",
+      width: size === "large" ? "90%" : "100%",
       height: 40,
       alignItems: "center",
       justifyContent: "center",
       backgroundColor: colors.secondary,
       paddingHorizontal: metrics.padding_lg,
-      borderRadius: metrics.rounded_base,
+      borderRadius: metrics.rounded_sm,
     },
     text: {
       color: colors.background,
-      fontSize: fonts.size.xl,
+      fontSize: size === "large" ? fonts.size.xl2 : fonts.size.xl,
       fontWeight: "700",
     },
   });
