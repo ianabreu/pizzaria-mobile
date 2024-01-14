63977d0bff8250a252a2063f182d67e627487abc
ianabreu <ianabreudev@gmail.com>
2024-01-14 03:35:04 -0300

    style: add prop size to InputProps on Input ui component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,23 @@
 import { StyleSheet, TextInput, TextInputProps } from "react-native";
 import { colors, fonts, metrics } from "../../theme";
 
-interface InputProps extends TextInputProps {}
-export function Input({ style, ...rest }: InputProps) {
+interface InputProps extends TextInputProps {
+  size?: "small" | "large";
+}
+export function Input({ size = "small", style, ...rest }: InputProps) {
   const styles = StyleSheet.create({
     input: {
-      width: "100%",
-      height: 40,
+      width: size === "large" ? "90%" : "100%",
+      height: size === "large" ? 60 : 40,
       paddingHorizontal: metrics.padding_lg,
       marginBottom: metrics.margin_lg,
-      borderWidth: 1,
-      borderRadius: metrics.rounded_base,
+      borderWidth: 0.5,
+      borderRadius: metrics.rounded_sm,
       borderColor: colors.placeholder,
       backgroundColor: colors.input,
       color: colors.foreground,
-      fontSize: fonts.size.lg,
+      fontSize: size === "large" ? fonts.size.xl2 : fonts.size.lg,
+      textAlign: "left",
     },
   });
   return (
