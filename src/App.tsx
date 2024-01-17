1b7316e64258ffb4782cbfcd6254de2286a7ce3a
ianabreu <ianabreudev@gmail.com>
2024-01-17 05:04:17 -0300

    chore: add react-native-toast-message and config of Toast mensages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,94 @@
 import React from "react";
+import Toast, {
+  BaseToast,
+  BaseToastProps,
+  ErrorToast,
+  InfoToast,
+} from "react-native-toast-message";
 import { SafeAreaView, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from "./routes";
-import { colors } from "./theme";
+import { colors, fonts, metrics } from "./theme";
 import { AuthProvider } from "./contexts/AuthContext";
+
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{
+        backgroundColor: colors.secondary,
+        borderColor: colors.secondary,
+        borderRadius: 4,
+        padding: 4,
+        height: "auto",
+        width: metrics.screenWidth - 32,
+      }}
+      text1Style={{
+        fontSize: fonts.size.xl2,
+        color: colors.foreground,
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+      text2Style={{
+        fontSize: fonts.size.xl2,
+        color: colors.foreground,
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+    />
+  ),
+  info: (props: BaseToastProps) => (
+    <InfoToast
+      {...props}
+      style={{
+        backgroundColor: colors.terciary,
+        borderColor: colors.terciary,
+        borderRadius: 4,
+        padding: 4,
+        height: "auto",
+        width: metrics.screenWidth - 32,
+      }}
+      text1Style={{
+        fontSize: fonts.size.xl2,
+        color: colors.foreground,
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+      text2Style={{
+        fontSize: fonts.size.xl2,
+        color: colors.foreground,
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+    />
+  ),
+
+  error: (props: BaseToastProps) => (
+    <ErrorToast
+      {...props}
+      style={{
+        backgroundColor: colors.error,
+        borderColor: colors.error,
+        borderRadius: 4,
+        padding: 4,
+        height: "auto",
+        width: metrics.screenWidth - 32,
+      }}
+      text1Style={{
+        fontSize: fonts.size.xl2,
+        color: colors.foreground,
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+      text2Style={{
+        fontSize: fonts.size.xl2,
+        color: colors.foreground,
+        fontWeight: "700",
+        textAlign: "center",
+      }}
+    />
+  ),
+};
 
 export default function App() {
   return (
@@ -15,6 +100,7 @@ export default function App() {
             backgroundColor={colors.background}
           />
           <Routes />
+          <Toast position="top" config={toastConfig} />
         </SafeAreaView>
       </AuthProvider>
     </NavigationContainer>
