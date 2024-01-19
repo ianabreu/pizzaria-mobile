df6f0bec08e0b1c816b5ae4d62da5fef4c49a48a
ianabreu <ianabreudev@gmail.com>
2024-01-19 20:18:58 -0300

    feat: add Dashboard screen and params route Order

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,93 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Keyboard } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import Toast from "react-native-toast-message";
+
+import { api } from "../../services/api";
+import { Button } from "../../components/ui/button";
+import { Container } from "../../components/layout/container";
+import { Input } from "../../components/ui/input";
+import { Title } from "../../components/ui/title";
+import { StackParamsList } from "../../routes/app.routes";
+import { metrics } from "../../theme";
+import { AxiosError } from "axios";
 
 export default function Dashboard() {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<StackParamsList>>();
+  const [table, setTable] = useState("");
+  const [loading, setLoading] = useState(false);
+  function handleSetTable(tableNumber: string) {
+    let numberOnly = tableNumber.replace(/\D/g, "");
+    if (numberOnly === "0") {
+      numberOnly = "";
+    }
+    if (numberOnly.startsWith("0")) {
+      let zeroOnLeft = Number(numberOnly);
+      numberOnly = String(zeroOnLeft);
+    }
+    setTable(numberOnly);
+  }
+
+  async function openOrder() {
+    if (table.trim() === "") {
+      Toast.show({
+        type: "error",
+        text1: "Informe uma mesa.",
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+      const response = await api.post(
+        "/order",
+        { table: Number(table) },
+        { timeout: 10000 },
+      );
+      const { table: tableNunber, id } = response.data;
+      navigation.navigate("order", {
+        table: tableNunber,
+        order_id: id,
+      });
+    } catch (error) {
+      if (error instanceof AxiosError && error.message === "Network Error") {
+        Toast.show({
+          type: "error",
+          text1: "Internet não disponível",
+        });
+        return;
+      }
+      Toast.show({
+        type: "error",
+        text1: "Erro ao abrir mesa",
+      });
+    } finally {
+      setLoading(false);
+      setTable("");
+      Keyboard.dismiss();
+    }
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: "green" }}>
-      <Text>Dashboard</Text>
-    </View>
+    <Container>
+      <Title style={{ marginBottom: metrics.margin_xlg }}>Novo Pedido</Title>
+      <Input
+        placeholder={"Número da mesa"}
+        keyboardType="numeric"
+        value={table}
+        onChangeText={handleSetTable}
+        selectTextOnFocus
+        size="large"
+        enterKeyHint="next"
+      />
+      <Button
+        size="large"
+        onPress={openOrder}
+        variant="secondary"
+        isLoading={loading}>
+        Abrir mesa
+      </Button>
+    </Container>
   );
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Dashboard from "../pages/Dashboard";
+import Order from "../pages/Order";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen } = createNativeStackNavigator<StackParamsList>();
+
+export type StackParamsList = {
+  dashboard: undefined;
+  order: {
+    table: number;
+    order_id: string;
+  };
+};
 
 export default function AppRoutes() {
   return (
     <Navigator>
-      <Screen name="dashboard" component={Dashboard} />
+      <Screen
+        name="dashboard"
+        component={Dashboard}
+        options={{ headerShown: false }}
+      />
+      <Screen name="order" component={Order} options={{ headerShown: false }} />
     </Navigator>
   );
 }
